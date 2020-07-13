74994502c6d6b18aef3a1bc7d1254d8c38fc9c39
Andy Booth <boog@planetofnoise.com>
2020-07-13 18:13:51 +0100

    Monday mids

--- test.js
+++ test.js
@@ -3,15 +3,23 @@ function sayhi(person = "Stranger")
 var hello="Hello ";
 document.write(hello+person+"\n");
 }
+
+function gender(person)
+{
+if(person=="Boogie"){
+  document.write("Boy\n")
+} else if (person=="Lucy") {
+  document.write("Girl\n")
+} else{
+  document.write("?\n")
+}}
+
 person="Boogie"
 sayhi(person);
+gender(person)
+
 person="Lucy"
 sayhi(person);
+gender(person)
+
 sayhi();
-if(person=="Boogie"){
-  document.write("Boy")
-} else if (person=="Lucy") {
-  document.write("Girl")
-} else{
-  document.write("Stranger")
-}
